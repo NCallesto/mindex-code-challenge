92c0b10011b3893c98bee6989aca0665d6a6a7ec
Noah Callesto <nrc3290@rit.edu>
2025-03-28 21:30:44 -0400

    Updated eployee service to add employee not found exception

--- src/main/java/com/mindex/challenge/service/EmployeeService.java
+++ src/main/java/com/mindex/challenge/service/EmployeeService.java
@@ -2,11 +2,22 @@ package com.mindex.challenge.service;
 
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.exception.EmployeeNotFoundException;
 
+/**
+ * Service interface for employee operations.
+ */
 public interface EmployeeService {
     Employee create(Employee employee);
-    Employee read(String id);
-    Employee update(Employee employee);
+    Employee read(String id) throws EmployeeNotFoundException;
+    Employee update(Employee employee) throws EmployeeNotFoundException;
 
-    ReportingStructure readReportingStructure(String employeeID);
+    /**
+     * Retrieves the complete reporting structure for a given employee.
+     * 
+     * @param employeeID The ID of the employee to get the reporting structure
+     * @return A ReportingStructure object with the employee and their total reports
+     * @throws EmployeeNotFoundException incase the employee is not found
+     */
+    ReportingStructure getReportingStructure(String employeeID) throws EmployeeNotFoundException;
 }
