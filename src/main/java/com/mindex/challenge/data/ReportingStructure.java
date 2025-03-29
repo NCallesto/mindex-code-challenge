4f962a176e5bfa7b855d6227e384e3e889fa5836
Noah Callesto <nrc3290@rit.edu>
2025-03-28 20:15:44 -0400

    Added documentation to the reporting struture class

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -1,29 +1,56 @@
 package com.mindex.challenge.data;
 
+/**
+ * Reporting Structure represents the reporting structure for an employee, including the employee details
+ * and total number of reports under them in the hierarchy.
+ */
 public class ReportingStructure {
     private Employee employee;
     private int numberOfReports;
 
-    public ReportingStructure() {
-    }
+    /**
+     * Default constructor.
+     */
+    public ReportingStructure() {}
 
+    /**
+     * Constructs a ReportingStructure with the given employee and report count.
+     *
+     * @param employee The employee for which the reporting structure is being created
+     * @param numberOfReports The total number of defined reports under this employee
+     */
     public ReportingStructure(Employee employee, int numberOfReports) {
         this.employee = employee;
         this.numberOfReports = numberOfReports;
     }
 
+
+    // Getters and Setters with JavaDoc
+
+    /**
+     * @return The employee this reporting structure refers to
+     */
     public Employee getEmployee() {
         return employee;
     }
 
+    /**
+     * @param employee The employee to set
+     */
     public void setEmployee(Employee employee) {
         this.employee = employee;
     }
 
+    /**
+     * @return The total number of defined reports under this employee
+     */
     public int getNumberOfReports() {
         return numberOfReports;
     }
 
+    /**
+     * @param numberOfReports The number of reports to set
+     */
     public void setNumberOfReports(int numberOfReports) {
         this.numberOfReports = numberOfReports;
     }
