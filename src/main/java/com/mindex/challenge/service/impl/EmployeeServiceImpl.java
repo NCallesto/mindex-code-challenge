6d1deac681ee6cf17a9fa3e71e68dd27fb6d28d9
Noah Callesto <nrc3290@rit.edu>
2025-03-28 21:35:55 -0400

    Updated eployee service impl to throw the employee not found exception

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,12 +2,14 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
+import com.mindex.challenge.exception.EmployeeNotFoundException;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -28,7 +30,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public Employee read(String id) {
+    public Employee read(String id) throws EmployeeNotFoundException {
         LOG.debug("Creating employee with id [{}]", id);
 
         Employee employee = employeeRepository.findByEmployeeId(id);
@@ -41,9 +43,50 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public Employee update(Employee employee) {
+    public Employee update(Employee employee) throws EmployeeNotFoundException {
         LOG.debug("Updating employee [{}]", employee);
 
+        // Check that the employee exists before updating
+        if (!employeeRepository.existsById(employee.getEmployeeId())) {
+            throw new EmployeeNotFoundException("Employee not found for update: " + employee.getEmployeeId());
+        }
+
         return employeeRepository.save(employee);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ReportingStructure getReportingStructure(String employeeID) throws EmployeeNotFoundException {
+        LOG.debug("Creating reporting structure for employee with id [{}]", employeeID);
+        
+        // Get the employee or throw EmployeeNotFoundException if not found
+        Employee employee = read(employeeID);
+        
+        // Calculate total reports recursively with the calculateNumberOfReports helper method
+        int numberOfReports = calculateNumberOfReports(employee);
+        
+        return new ReportingStructure(employee, numberOfReports);
+    }
+
+    /**
+     * Recursively calculates the total number of reports under an employee.
+     * 
+     * @param employee The employee to calculate reports for
+     * @return The total number of reports
+     */
+    private int calculateNumberOfReports(Employee employee) {
+        // Base case: there are no direct reports
+        if (employee.getDirectReports() == null) {
+            return 0;
+        }
+        
+        int count = 0;
+        for (Employee directReport : employee.getDirectReports()) {
+            // Count this employee plus all of their reports
+            count += 1 + calculateNumberOfReports(directReport);
+        }
+        return count;
     }
 }
