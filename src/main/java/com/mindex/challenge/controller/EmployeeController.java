da4f460176598e4dc02af7115a390b48a9f1d3c3
Noah Callesto <nrc3290@rit.edu>
2025-03-29 22:12:39 -0400

    Updated EmployeeController to keep the geReportingStructure method and added request mapping

--- src/main/java/com/mindex/challenge/controller/EmployeeController.java
+++ src/main/java/com/mindex/challenge/controller/EmployeeController.java
@@ -1,7 +1,5 @@
 package com.mindex.challenge.controller;
 
-import com.mindex.challenge.data.Employee;
-import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +8,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.EmployeeService;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
 import com.mindex.challenge.exception.EmployeeNotFoundException;
 
 /**
  * REST controller for employee-related operations.
- * Exposes endpoints for CRUD operations.
+ * Exposes endpoints for CRUD operations and reporting structure.
  */
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
+    private final EmployeeService employeeService;
+    private final ReportingStructureService reportingStructureService;
+
     @Autowired
-    private EmployeeService employeeService;
+    public EmployeeController(EmployeeService employeeService, 
+                            ReportingStructureService reportingStructureService) {
+        this.employeeService = employeeService;
+        this.reportingStructureService = reportingStructureService;
+    }
 
     /**
      * Creates a new employee.
@@ -30,7 +42,7 @@ public class EmployeeController {
      * @param employee Employee data to create
      * @return The created employee with generated ID
      */
-    @PostMapping("/employee")
+    @PostMapping("")
     public Employee create(@RequestBody Employee employee) {
         LOG.debug("Received employee create request for [{}]", employee);
 
@@ -44,7 +56,7 @@ public class EmployeeController {
      * @return The requested employee
      * @throws EmployeeNotFoundException if no employee exists with the given ID
      */
-    @GetMapping("/employee/{id}")
+    @GetMapping("/{id}")
     public Employee read(@PathVariable String id) throws EmployeeNotFoundException {    
         LOG.debug("Received employee read request for id [{}]", id);
 
@@ -59,11 +71,26 @@ public class EmployeeController {
      * @return The updated employee
      * @throws EmployeeNotFoundException if no employee exists with the given ID
      */
-    @PutMapping("/employee/{id}")
+    @PutMapping("/{id}")
     public Employee update(@PathVariable String id, @RequestBody Employee employee) throws EmployeeNotFoundException {
         LOG.debug("Received employee create request for id [{}] and employee [{}]", id, employee);
 
         employee.setEmployeeId(id);
         return employeeService.update(employee);
+    }
+
+    /**
+     * Retrieves the complete reporting structure hierarchy for an employee.
+     * 
+     * @param id The ID of the employee to get the reporting structure for
+     * @return Fully populated reporting structure with all direct and indirect reports
+     * @throws EmployeeNotFoundException if no employee exists with the given ID
+     * @apiNote Consider this part of the employee resource, hence the URL structure
+     * @example GET /employee/16a596ae-edd3-4847-99fe-c4518e82c86f/reporting-structure
+     */
+    @GetMapping("/{id}/reporting-structure")
+    public ReportingStructure getReportingStructure(@PathVariable String id) throws EmployeeNotFoundException {
+        LOG.debug("Received reporting structure request for employee id [{}]", id);
+        return reportingStructureService.getReportingStructure(id);
     }
 }
