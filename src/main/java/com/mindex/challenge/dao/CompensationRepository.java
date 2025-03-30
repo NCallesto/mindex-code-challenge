38dd43f833feca626b044c5bcaad7df389ec240f
Noah Callesto <nrc3290@rit.edu>
2025-03-30 19:16:39 -0400

    Added Compensation Repository ass

---
+++ src/main/java/com/mindex/challenge/dao/CompensationRepository.java
@@ -0,0 +1,21 @@
+package com.mindex.challenge.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mindex.challenge.data.Compensation;
+
+/**
+ * MongoDB repository for Compensation entities.
+ * Provides basic CRUD operations and custom query methods for Compensation data.
+ */
+@Repository
+public interface CompensationRepository extends MongoRepository<Compensation, String> {
+    /**
+     * Finds compensation by employee ID.
+     *
+     * @param employeeId The ID of the employee to find compensation for
+     * @return The Compensation object or null if not found
+     */
+    Compensation findByEmployeeEmployeeId(String employeeId);
+}
