c5e5ba810167e0fe62520a9c521909314a0a6b7d
Noah Callesto <nrc3290@rit.edu>
2025-03-31 01:36:25 -0400

    Initial test setup

--- src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
@@ -1,16 +1,54 @@
 package com.mindex.challenge;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.ReportingStructure;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChallengeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@LocalServerPort
+    private int port;
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    private String employeeUrl;
+    private String reportingStructureUrl;
+    private String compensationUrl;
+
+    @Before
+    public void setup() {
+		// Set up each urls for the different endpoints
+        employeeUrl = "http://localhost:" + port + "/employee";
+        reportingStructureUrl = "http://localhost:" + port + "/employee/{id}/reporting-structure";
+        compensationUrl = "http://localhost:" + port + "/employee/{id}/compensation";
+    }
+
+	@Test
+    public void testReportingStructure() {
+
+    }
+
+	@Test
+    public void testCompensationCreateRead() {
+
+    }
+
+	@Test
+    public void testDirectReportsNotNullForLeaves() {
+		
+	}
 }
